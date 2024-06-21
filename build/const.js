a9578677544033ec40e7212ee6526171391fd29a
Cédric Uden <cedric@uden.de>
2024-06-21 20:36:15 +0200

    remove and ignore build files

--- build/const.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LIGHT_MODE_16BIT = exports.LIGHT_MODE_8BIT_DIMMABLE = exports.LIGHT_MODE_8BIT = void 0;
-exports.LIGHT_MODE_8BIT = '8bit';
-exports.LIGHT_MODE_8BIT_DIMMABLE = '8bit-dimmable';
-exports.LIGHT_MODE_16BIT = '16bit';
-//# sourceMappingURL=const.js.map
