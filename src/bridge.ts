5ce0a5b933c356012feeff97726e82c57017c060
Luca Heß <development@luca-hess.de>
2024-11-07 20:30:16 +0100

    transferred light classes to own file

--- src/bridge.ts
+++ src/bridge.ts
@@ -1,9 +1,10 @@
 import {v3} from 'node-hue-api';
 import {Api} from 'node-hue-api/dist/esm/api/Api';
-import {ChannelModeType, LIGHT_MODE_16BIT, LIGHT_MODE_8BIT, LIGHT_MODE_8BIT_DIMMABLE} from './const';
+ import {ChannelModeType} from './const';
 import {ColorUpdate, HueDtlsController} from './hue-dtls';
 import {ArtNetController} from 'artnet-protocol/dist';
 import {ArtDmx} from 'artnet-protocol/dist/protocol';
+import {DmxLight, LIGHT_MODES} from "./DmxLight";
 
 export interface LightConfiguration {
     dmxStart: number;
@@ -19,78 +20,6 @@ export interface Configuration {
     artNetBindIp: string;
     artNetUniverse: number;
     lights: LightConfiguration[];
-}
-
-abstract class DmxLight {
-
-    readonly dmxStart: number;
-    readonly lightId: number;
-
-    constructor(dmxStart: number, lightId: number) {
-        this.dmxStart = dmxStart;
-        this.lightId = lightId;
-    }
-
-    /**
-     * The amount of DMX channels this mode takes up.
-     */
-    abstract channelWidth: number;
-
-    /**
-     * Convert the raw DMX data to list containing 16bit RGB values.
-     *
-     * The input is an array with length equal to `channelWidth`, which contains
-     * the values of the DMX channels representing the light.
-     * The implementer is responsible for converting the DMX values to 16bit unsigned RGB.
-     *
-     * @param dmxValues The raw DMX values of the channels representing this light.
-     */
-    abstract getColorValue(dmxValues: number[]): [number, number, number];
-}
-
-class DmxLight8Bit extends DmxLight {
-
-    channelWidth = 3;
-
-    getColorValue(dmxValues: number[]): [number, number, number] {
-        const r = (dmxValues[0] * 257);
-        const g = (dmxValues[1] * 257);
-        const b = (dmxValues[2] * 257);
-        return [r, g, b];
-    }
-}
-
-class DmxLight8BitDimmable extends DmxLight {
-
-    channelWidth = 4;
-
-    getColorValue(dmxValues: number[]): [number, number, number] {
-        const r = (dmxValues[1] * 257) * (dmxValues[0] / 255);
-        const g = (dmxValues[2] * 257) * (dmxValues[0] / 255);
-        const b = (dmxValues[3] * 257) * (dmxValues[0] / 255);
-        return [r, g, b];
-    }
-}
-
-class DmxLight16Bit extends DmxLight {
-
-    channelWidth = 6;
-
-    getColorValue(dmxValues: number[]): [number, number, number] {
-        const r = dmxValues[0];
-        const rFine = dmxValues[1];
-        const g = dmxValues[2];
-        const gFine = dmxValues[3];
-        const b = dmxValues[4];
-        const bFine = dmxValues[5];
-        return [(r << 8) + rFine, (g << 8) + gFine, (b << 8) + bFine];
-    }
-}
-
-const LIGHT_MODES = {
-    [LIGHT_MODE_8BIT]: DmxLight8Bit,
-    [LIGHT_MODE_8BIT_DIMMABLE]: DmxLight8BitDimmable,
-    [LIGHT_MODE_16BIT]: DmxLight16Bit,
 }
 
 export class ArtNetHueBridge {
